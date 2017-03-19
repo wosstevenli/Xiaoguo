c6cab2657113cea2770d7564a424277425efc728
STEVEN LI <wosstevenli@gmail.com>
2017-03-19 11:18:50 +0800

    整理小功能
    整理小功能

---
+++ sw/js/1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Steven on 2016/5/17.
+ */
+$('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
