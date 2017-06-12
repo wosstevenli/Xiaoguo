51ab8fceddc086e3128254b65c6a8acbed859461
STEVEN LI <wosstevenli@gmail.com>
2017-06-12 19:30:16 +0800

    20170612json
    20170612json

--- json/js/js01.js
+++ json/js/js01.js
@@ -83,7 +83,8 @@ document.getElementById("xunhuan").innerHTML = x;*/
 
 /*var JsonObject = JSON.parse('{"name":"STEVEN","job":"DESIGNER","city":"WUHAN"}');
 document.getElementById("div001").innerHTML = JsonObject.name + ":" + JsonObject.job + ":" + JsonObject.city;*/
-var JsonObject = "";
+
+/*var JsonObject = "";
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status === 200) {
@@ -91,5 +92,61 @@ xmlhttp.onreadystatechange = function() {
         document.getElementById("div001").innerHTML = JsonObject.name;
     }
 };
-xmlhttp.open("GET", "20170611.txt", true);
-xmlhttp.send();
+// xmlhttp.open("GET", "20170611.txt", true);  //从服务端接收数组的 JSON 数据
+xmlhttp.open("GET", "20170611.json", true); //也可以直接是json文件
+xmlhttp.send();*/
+
+/*var JsonObject = "";
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status === 200) {
+        JsonObject = JSON.parse(this.responseText);
+        document.getElementById("div001").innerHTML = JsonObject[0];
+    }
+};
+xmlhttp.open("GET", "2017061101.txt", true);
+xmlhttp.send();*/
+
+/*var text = '{"name":"JOBS","date":"2017-06-11","list":"learn json"}';
+//var text = '{"name":"JOBS","date":"Sun Jun 11 2017 08:00:00 GMT+0800 (中国标准时间)","list":"learn json"}';
+var JsonObject = JSON.parse(text);
+JsonObject.date = new Date(JsonObject.date); //没有这个则只会输入原始的2017-06-11相当于字符串输出，有则会输出日期时间星期等信息
+document.getElementById("div001").innerHTML = JsonObject.name + ":" + JsonObject.date + "/" + JsonObject.list;*/
+
+/*var text = '{"name":"JOBS","date":"2017-06-11","list":"learn json"}';
+var JsonObject = JSON.parse(text, function(key, value) {
+    if (key == "date") { //用什么表示都可以，但为了不引起误解，还是用initDate比较正规合适
+        return new Date(value);
+    } else {
+        return value;
+    }
+});
+document.getElementById("div001").innerHTML = JsonObject.name + ":" + JsonObject.date + "/" + JsonObject.list;*/
+
+/*var text = '{"name":"JOBS","initDate":"2017-06-11","list":"learn json"}';
+var JsonObject = JSON.parse(text, function(key, value) {
+    if (key == "initDate") { //用什么表示都可以，但为了不引起误解，还是用initDate比较正规合适
+        return new Date(value);
+    } else {
+        return value;
+    }
+});
+document.getElementById("div001").innerHTML = JsonObject.name + ":" + JsonObject.initDate + "/" + JsonObject.list;*/
+
+/*var text = '{"name":"JOBS","paiming":"function(){return 10000;}","list":"learn json"}';
+var JsonObject = JSON.parse(text);
+JsonObject.paiming = eval("(" + JsonObject.paiming + ")");
+document.getElementById("div001").innerHTML = JsonObject.name + "排名是：" + JsonObject.paiming() + "/" + JsonObject.list;*/
+
+/*var obj = { "name": "STEVEN", "alexa": 189, "title": "Designer" };
+var myJson = JSON.stringify(obj);
+document.getElementById("div001").innerHTML = myJson;*/
+
+/*var arr = ["Google", "facebook", "twitter"];
+var myJson = JSON.stringify(arr);//这个好像就是处理json和数组对象的函数
+document.getElementById("div001").innerHTML = myJson;*/
+
+//var arr = ["Google", "facebook", "twitter"];
+//var obj = { "name": "STEVEN", "alexa": 189, "title": "Designer" };
+//var myJson = JSON.stringify(arr);//这个好像就是处理json和数组对象的函数
+//document.getElementById("div001").innerHTML = obj; //这个还必须经过stringify来处理输入，不然输出的将是字符串
